73d04e6ab28f9244bb3f2b058cee4ba5ac755281
Valentin Lorentz <progval@progval.net>
2014-10-29 16:56:02 +0100

    Revert "Install requests first so pip can run safely."
    This reverts commit 576e5a8ec889d970d9787b2941e07a4e0c46e71a.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip3 install --user requests # pip itself may require requests.
 pip3 install --user --upgrade routes ppp_core
 pip3 install --user --upgrade git+https://github.com/ProjetPP/ExamplePPPModule-Python.git
