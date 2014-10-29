02df9a26366c5fc7dc3bb98981b2db4d79d7b4fb
Valentin Lorentz <progval@progval.net>
2014-10-29 16:40:00 +0100

    bootstrap script can upgrade modules too.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip3 install --user routes ppp_core
-pip3 install --user git+https://github.com/ProjetPP/ExamplePPPModule-Python.git
+pip3 install --user --upgrade routes ppp_core
+pip3 install --user --upgrade git+https://github.com/ProjetPP/ExamplePPPModule-Python.git
