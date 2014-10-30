c2b1a2c6aa5e3b6da71cd910c4070d52bfd8c9c3
Valentin Lorentz <progval@progval.net>
2014-10-30 18:02:05 +0100

    Fix CoreNLP deployment.

--- run_corenlp.sh
+++ run_corenlp.sh
@@ -1,1 +1,1 @@
-CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\" -Xmx500m" CORENLP=stanford-corenlp-full-* python3 -m corenlp --m 1g
+CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\" -Xmx500m" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g
