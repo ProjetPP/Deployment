a0d867032bdbd9ea46f52696cc1c7f6827ca1d3b
Valentin Lorentz <progval@progval.net>
2014-11-13 17:20:24 +0100

    Prevent the dispatcher from crashing if an application crashed.

--- python_dispatcher.py
+++ python_dispatcher.py
@@ -1,8 +1,10 @@
+import traceback
 from routes import Mapper
 
 import ppp_core
 import example_ppp_module as flower
 import ppp_nlp_classical
+#import ppp_nlp_ml_standalone
 
 class Application:
     def __init__(self):
@@ -10,11 +12,17 @@ class Application:
         self.mapper.connect('core', '/core/', app=ppp_core.app)
         self.mapper.connect('nlp_classical', '/nlp_classical/', app=ppp_nlp_classical.app)
         self.mapper.connect('flower', '/flower/', app=flower.app)
+        #self.mapper.connect('nlp_ml_standalone', '/nlp_ml_standalone/', app=ppp_nlp_ml_standalone.app)
 
     def __call__(self, environ, start_response):
         match = self.mapper.routematch(environ=environ)
         app = match[0]['app'] if match else self.not_found
-        return app(environ, start_response)
+        try:
+            return app(environ, start_response)
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            traceback.print_exc(e)
 
     def not_found(self, environ, start_response):
         headers = [('Content-Type', 'text/plain')]
