05c653abfa987888ac48bfbfcc7e5eb7a4070963
Valentin Lorentz <progval@progval.net>
2014-11-16 16:04:43 +0100

    Use a mirror for downloading CoreNLP (nlp.stanford.edu is often down).

--- bootstrap_corenlp.sh
+++ bootstrap_corenlp.sh
@@ -4,7 +4,7 @@ pip3 install --user --upgrade git+https://bitbucket.org/ProgVal/corenlp-python.g
 if [ ! -f stanford-corenlp-full-2014-08-27.zip ]
 then
     echo "Downloading CoreNLP (long: 221MB)…"
-    wget http://nlp.stanford.edu/software/stanford-corenlp-full-2014-08-27.zip
+    wget http://static.progval.net/public/stanford-corenlp-full-2014-08-27.zip
 fi
 echo "Extracting CoreNLP…"
 rm -rf stanford-corenlp-full-2014-08-27
