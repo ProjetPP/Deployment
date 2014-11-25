33ed250918373cfaf4220af95183a23d7f32253a
Valentin Lorentz <progval@progval.net>
2014-11-25 11:03:08 +0100

    Use the new name of QuestionParsing_Grammatical.

--- python_dispatcher.py
+++ python_dispatcher.py
@@ -3,7 +3,7 @@ from routes import Mapper
 
 import ppp_core
 import example_ppp_module as flower
-import ppp_nlp_classical
+import ppp_questionparsing_grammatical as qp_grammatical
 import ppp_cas
 #import ppp_nlp_ml_standalone
 
@@ -11,7 +11,7 @@ class Application:
     def __init__(self):
         self.mapper = Mapper()
         self.mapper.connect('core', '/core/', app=ppp_core.app)
-        self.mapper.connect('nlp_classical', '/nlp_classical/', app=ppp_nlp_classical.app)
+        self.mapper.connect('qp_grammatical', '/qp_grammatical/', app=qp_grammatical.app)
         self.mapper.connect('flower', '/flower/', app=flower.app)
         self.mapper.connect('cas', '/cas/', app=ppp_cas.app)
         self.mapper.connect('spellcheck', '/spell_checker/', app=ppp_cas.app)
