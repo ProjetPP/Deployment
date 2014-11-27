47268ae771859fd1d276cb9022312d8639312878
Thomas Tanon <Tpt@users.noreply.github.com>
2014-11-27 09:57:34 +0100

    + pppLoggerUrl

--- webui_config.js
+++ webui_config.js
@@ -1,3 +1,4 @@
 window.config = {
-    pppCoreUrl: 'http://gunicorn.ppp.pony.ovh/core/'
-}
+    pppCoreUrl: 'http://gunicorn.ppp.pony.ovh/core/',
+    pppLoggerUrl: 'http://gunicorn9005.ppp.pony.ovh/'
+};
