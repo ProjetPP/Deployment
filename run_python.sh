47ef2395e3ed8cf7f30e8766129fa4bc8997f4ae
Valentin Lorentz <progval@progval.net>
2014-12-04 15:20:43 +0100

    Optimizations.

--- run_python.sh
+++ run_python.sh
@@ -6,10 +6,10 @@ export PPP_NLP_CLASSICAL_CONFIG=nlp_classical_config.json
 export PPP_CAS_CONFIG=cas_config.json
 export PPP_LOGGER_CONFIG=logger_config.json
 export PPP_ML_STANDALONE_CONFIG=qp_ml_standalone_config.json
-gunicorn ppp_core:app -b 0.0.0.0:9000 -w 10 -t 120 &
+gunicorn ppp_core:app -b 0.0.0.0:9000 -w 4 -t 60 --preload &
 #gunicorn ppp_questionparsing_grammatical:app -b 0.0.0.0:9001 -w 4 &
 #gunicorn example_ppp_module:app -b 0.0.0.0:9002 -w 4 &
-gunicorn ppp_cas:app -b 0.0.0.0:9003 -w 4 &
+gunicorn ppp_cas:app -b 0.0.0.0:9003 -w 4 --preload &
 #gunicorn ppp_spell_checker:app -b 0.0.0.0:9004 -w 4 &
-gunicorn ppp_logger:app -b 0.0.0.0:9005 -w 4 &
-gunicorn ppp_questionparsing_ml_standalone:app -b 0.0.0.0:9006 -w 4 &
+gunicorn ppp_logger:app -b 0.0.0.0:9005 -w 4 --preload &
+gunicorn ppp_questionparsing_ml_standalone:app -b 0.0.0.0:9006 -w 4 --preload &
