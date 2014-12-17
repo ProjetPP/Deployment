e7af65517b4264c7d09857767f05f4c15ed22586
Thomas Tanon <Tpt@users.noreply.github.com>
2014-12-17 15:09:30 +0100

    + icon PNG of size 192

--- bootstrap_icons.sh
+++ bootstrap_icons.sh
@@ -12,6 +12,7 @@ convert tmp-img/icon-16.png tmp-img/icon-32.png tmp-img/icon-48.png flavicon.ico
 #icons
 convert -background none -geometry 64 icon.svg img/icon-64.png
 convert -background none -geometry 152 icon.svg img/icon-152.png
+convert -background none -geometry 192 icon.svg img/icon-192.png
 cp icon.svg img/icon.svg
 
 rm -r ./tmp-img
