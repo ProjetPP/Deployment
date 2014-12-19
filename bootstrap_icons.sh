b5155231e86df91b00d64efac0258053756117c0
Thomas Tanon <Tpt@users.noreply.github.com>
2014-12-19 18:29:54 +0100

    Use rsvg to do rendering

--- bootstrap_icons.sh
+++ bootstrap_icons.sh
@@ -4,15 +4,15 @@ mkdir -p img
 mkdir tmp-img
 
 #flavicon.ico
-convert -background none -colors 8 -geometry 16 icon.svg tmp-img/icon-16.png
-convert -background none -colors 8 -geometry 32 icon.svg tmp-img/icon-32.png
-convert -background none -colors 8 -geometry 48 icon.svg tmp-img/icon-48.png
+rsvg -w 16 icon.svg tmp-img/icon-16.png
+rsvg -w 32 icon.svg tmp-img/icon-32.png
+rsvg -w 48 icon.svg tmp-img/icon-48.png
 convert tmp-img/icon-16.png tmp-img/icon-32.png tmp-img/icon-48.png flavicon.ico
 
 #icons
-convert -background none -geometry 64 icon.svg img/icon-64.png
-convert -background none -geometry 152 icon.svg img/icon-152.png
-convert -background none -geometry 192 icon.svg img/icon-192.png
+rsvg -w 64 icon.svg img/icon-64.png
+rsvg -w 152 icon.svg img/icon-152.png
+rsvg -w 192 icon.svg img/icon-192.png
 cp icon.svg img/icon.svg
 
 rm -r ./tmp-img
