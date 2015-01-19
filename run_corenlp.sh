b31723911fd75821402453d5fa8eef64592eddcb
Valentin Lorentz <progval@progval.net>
2015-01-20 00:29:19 +0100

    New config of Grammatical Question Parser.

--- run_corenlp.sh
+++ run_corenlp.sh
@@ -1,1 +1,3 @@
-CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g -p 8999
+CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g -p 8999 &
+CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g -p 8998 &
+for job in `jobs -p`; do echo $job;     wait $job; done
