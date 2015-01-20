631dc4cb535f001c9fe983df7e9a3eae68f8452c
Valentin Lorentz <progval@progval.net>
2015-01-20 09:50:45 +0100

    Fix run on Travis (lack of memory).

--- run_corenlp.sh
+++ run_corenlp.sh
@@ -1,3 +1,2 @@
 CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g -p 8999 &
-CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g -p 8998 &
 for job in `jobs -p`; do echo $job;     wait $job; done
