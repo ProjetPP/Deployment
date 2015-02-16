7d1c68f74c48fd75fcd7cac8bf94c4a3445f0a2c
Valentin Lorentz <progval@progval.net>
2015-02-16 09:17:04 +0100

    Use Oracle's VM if OpenJDK's is not available.

--- run_corenlp.sh
+++ run_corenlp.sh
@@ -1,2 +1,10 @@
-JAVA_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g -p 8999 &
+export JAVA_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+
+if [ ! -f $JAVA_PATH ]
+then
+    export JAVA_PATH=/usr/lib/jvm/java-8-oracle/bin/java
+fi
+
+
+CORENLP_OPTIONS="-parse.flags \" -makeCopulaHead\"" CORENLP=stanford-corenlp-full-* python3 -m corenlp --memory 1g -p 8999 &
 for job in `jobs -p`; do echo $job;     wait $job; done
