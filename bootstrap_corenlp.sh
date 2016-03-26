6341c6f06acbb6a2e6f0af993b782d28539ef4c7
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-03-26 19:31:08 +0100

    Update bootstrap_corenlp.sh to install the latest version of CoreNLP.

--- bootstrap_corenlp.sh
+++ bootstrap_corenlp.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
-USER_MODE="--user"
-if [ $# -eq 1 ] && [ $1 == "--nouser" ]
+if [ ! -d CoreNLP ]
 then
-    echo "No user mode."
-    USER_MODE=""
+    echo "Cloning and installing CoreNLP…"
+    git clone git@github.com:stanfordnlp/CoreNLP.git
+    cd CoreNLP
+    ant compile
+    ant jar
+    cd ..
 fi
-
-python3 -m pip install pexpect unidecode xmltodict jsonrpclib-pelix $USER_MODE
-
-if [ ! -f corenlp-python ]
+if ! ls stanford-english-corenlp-*models.jar 1> /dev/null 2>&1
 then
-    echo "Cloning Valentin's modified corenlp-python library…"
-    git clone https://bitbucket.org/ProgVal/corenlp-python.git
+    echo "Downloading English model for CoreNLP…"
+    wget http://nlp.stanford.edu/software/stanford-english-corenlp-2016-01-10-models.jar
 fi
-echo "Installing it…"
-cd corenlp-python
-python3 setup.py install $USER_MODE
-cd ..
-if [ ! -f stanford-corenlp-full-2015-01-29.zip ]
-then
-    echo "Downloading CoreNLP (long: 221MB)…"
-    wget http://nlp.stanford.edu/software/stanford-corenlp-full-2015-01-29.zip
-fi
-echo "Extracting CoreNLP…"
-rm -rf stanford-corenlp-full-2015-01-29
-unzip stanford-corenlp-full-2015-01-29.zip
+yes | cp -l stanford-english-corenlp-*models.jar CoreNLP
 echo "All seemed to work. Hold tight while we test it on a simple example (might take some time)."
-CORENLP=stanford-corenlp-full-2015-01-30 python3 -c "print(repr(__import__('corenlp').StanfordCoreNLP().raw_parse('This is a sentence.')))"
-
-if [ ! -f stanford-postagger-full-2014-10-26.zip ]
+cd CoreNLP
+export CLASSPATH="`find . -name '*.jar'`"
+java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer &
+SERVER_PID=$!
+sleep 1 # Let the server some time to start...
+wget --post-data 'the quick brown fox jumped over the lazy dog' 'localhost:9000/?properties={"annotators": "tokenize,ssplit,pos", "outputFormat": "json"}' -O -
+if [ $? -ne 0 ]
 then
-    echo "Downloading POS Tagger (long: 123MB)…"
-    wget http://nlp.stanford.edu/software/stanford-postagger-full-2014-10-26.zip
+    echo "Something does not work…"
+else
+    echo "All seemed to work!"
 fi
-echo "Installing POS Tagger"
-unzip stanford-postagger-full-2014-10-26.zip
-python3 -c "import nltk; nltk.download('wordnet'); nltk.download('omw')"
+kill $SERVER_PID
